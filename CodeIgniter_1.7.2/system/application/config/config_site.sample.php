955f8a8c56d9b306723241fb09150ae7f6e24484
Beau D. Simensen <beau@dflydev.com>
2010-04-16 18:45:05 -0700

    o Sample site files so people can look at them.

---
+++ CodeIgniter_1.7.2/system/application/config/config_site.sample.php
@@ -0,0 +1,3 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+// Use this file to override the config defaults on a site-by-site basis.
+$config['base_url'] = "http://altern8-workspaces.portishead.dflydev.com/repose-ci-php-demo/CodeIgniter_1.7.2/";
---
+++ CodeIgniter_1.7.2/system/application/config/database_site.sample.php
@@ -0,0 +1,6 @@
+<?php
+// Use this file to override the database defaults on a site-by-site basis.
+$db['default']['username'] = "root";
+$db['default']['password'] = "password";
+$db['default']['database'] = "repose-ci-php-demo";
+?>
